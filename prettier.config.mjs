8e8bd4eb145a14565afa6829120b832434f24339
Robin Scheper <robin.scheper@gmail.com>
2025-09-08 15:23:11 +0200

    add Prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+export default {
+    tabWidth: 4, // Use 4 spaces for indentation
+    semi: true, // Add semicolons at the end of statements
+    singleQuote: true, // Use single quotes instead of double quotes
+    bracketSpacing: true, // Add spaces between brackets in object literals and imports
+    trailingComma: 'es5', // Add trailing commas where valid in ES5
+    printWidth: 100, // Line width
+    arrowParens: 'always', // Always add parentheses around arrow function parameters
+    endOfLine: 'auto', // Maintain existing line endings
+};
