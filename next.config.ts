49f413664e34bceaab1211d478499d11176904ba
Robin Scheper <robin.scheper@gmail.com>
2025-09-09 14:47:37 +0200

    add new remote pattern for images in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
-import type { NextConfig } from 'next';
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    images: {
-        remotePatterns: [new URL('http://localhost:54321/storage/**')],
-    },
+	/* config options here */
+	images: {
+		remotePatterns: [
+			new URL('http://localhost:54321/storage/**'),
+			new URL('https://api.pixelports.com/storage/**')
+		],
+	},
 };
 
 export default nextConfig;
