a03c739e6c845cfa31fece0003b2da4c3b654c17
Robin Scheper <robin.scheper@gmail.com>
2025-10-16 19:58:53 +0200

    unused

--- app/ShotThumbnail.tsx
+++
@@ -1,5 +0,0 @@
-import Image from 'next/image';
-
-export const ShotThumbnail = ({ src, alt }: { src: string; alt: string }) => {
-    return <Image src={src} alt={alt} width={336} height={252} />;
-};
