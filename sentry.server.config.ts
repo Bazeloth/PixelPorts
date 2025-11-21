d86ab4e3d47f2d4d3757110cceb2f053b643c866
Robin Scheper <robin.scheper@gmail.com>
2025-11-21 18:54:16 +0100

    integrate Sentry for error monitoring: add client and server configuration, global error handler, example page, and API route for demonstration

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://bc3c3e43397b0c1f0a993e9eedb1502e@o4510404166221824.ingest.de.sentry.io/4510404168187984",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
